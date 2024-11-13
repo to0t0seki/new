8e74c27d9d4f8aca49c278a0542d926f9aa8f8c4
taka9999 <to0t0seki@gmail.com>
2024-11-13 16:13:29 +0900

    3

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
 import streamlit as st
 
-st.write('Hello World')
-print('Hello World')
-aa = 1
+app = FastAPI()
+
+class Data(BaseModel):
+    aa: int
+
+@app.get("/get_data", response_model=Data)
+def get_data():
+    aa = 1
+    print("Hello World")  # サーバーのコンソールに出力されます
+    return Data(aa=aa)
+
+st.write("Hello World")
